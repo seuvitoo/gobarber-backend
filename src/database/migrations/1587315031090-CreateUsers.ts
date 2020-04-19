4cb960e99f892e3b1727f4d4698caa093899ce98
Victor dos Santos <vy.santos@live.com>
2020-04-19 14:50:27 -0300

    add table user and migration to edit

--- src/database/migrations/1587315031090-CreateUsers.ts
+++ src/database/migrations/1587315031090-CreateUsers.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1587004171480
-  implements MigrationInterface {
+export default class CreateUsers1587315031090 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -15,14 +14,18 @@ export default class CreateAppointments1587004171480
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
             isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
-            isNullable: false,
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
@@ -40,6 +43,6 @@ export default class CreateAppointments1587004171480
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }

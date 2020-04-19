915fa72327484f28aba784a1943aabf46d3da2ac
Victor dos Santos <vy.santos@live.com>
2020-04-19 15:42:05 -0300

    add password crypto

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { hash } from 'bcryptjs';
 
 import CreateUserService from '../services/CreateUserService';
 
@@ -9,11 +10,15 @@ usersRouter.post('/', async (request, response) => {
     const { name, email, password } = request.body;
     const createUser = new CreateUserService();
 
+    const hashedPassword = await hash(password, 8);
     const user = await createUser.execute({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
+
+    delete user.password;
+
     return response.json(user);
   } catch (err) {
     return response.status(400).json({ error: err.message });

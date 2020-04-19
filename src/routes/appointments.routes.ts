326127a103b7b0b5869b5cc92f5032cfab9e29df
Victor dos Santos <vy.santos@live.com>
2020-04-19 19:32:34 -0300

    global error handling

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -17,21 +17,17 @@ appointmentsRouter.get('/', async (request, response) => {
 });
 
 appointmentsRouter.post('/', async (request, response) => {
-  try {
-    const { provider_id, date } = request.body;
-    const parsedDate = parseISO(date);
+  const { provider_id, date } = request.body;
+  const parsedDate = parseISO(date);
 
-    const CreateAppointment = new CreateAppointmentService();
+  const CreateAppointment = new CreateAppointmentService();
 
-    const appointment = await CreateAppointment.execute({
-      date: parsedDate,
-      provider_id,
-    });
+  const appointment = await CreateAppointment.execute({
+    date: parsedDate,
+    provider_id,
+  });
 
-    return response.json(appointment);
-  } catch (err) {
-    return response.status(err.statusCode).json({ error: err.message });
-  }
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;

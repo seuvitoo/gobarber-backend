bbfe82e091ae54fb725352018d6e894f6908db93
Victor dos Santos <vy.santos@live.com>
2021-03-29 12:46:55 -0300

    [FEATURE] TDD - function send email

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -12,7 +12,7 @@ import '@shared/container';
 const app = express();
 
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory));
+//app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {

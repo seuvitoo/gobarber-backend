b8833ae14bb30a046c7f112585526623459ea6e3
Victor dos Santos <vy.santos@live.com>
2021-03-31 19:47:29 -0300

    [FEATURE] configurando template de e-mail

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -6,9 +6,17 @@ import DiskStoreProvider from './StoreProvider/implementations/DiskStoreProvider
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 import IMailProvider from './MailProvider/models/IMailProvider';
 
+import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
+
 container.registerSingleton<IStoreProvider>('StoreProvider', DiskStoreProvider);
+
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  HandlebarsMailTemplateProvider,
+);
 
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  new EtherealMailProvider(),
+  container.resolve(EtherealMailProvider)
 );

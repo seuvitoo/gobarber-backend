f08b0914f5507afbe8cc1ea6aeb3eca2dc3ddfd5
Victor dos Santos <vy.santos@live.com>
2021-04-13 18:32:20 -0300

    [FEATURE] add ListProviders

--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -1,7 +1,11 @@
-import User from '@modules/users/infra/typeorm/entities/User';
+import { uuid } from 'uuidv4';
+
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
-import { uuid } from 'uuidv4';
+
+import IFindAllProvidersDTO from '@modules/users/dtos/IFindAllProvidersDTO';
+
+import User from '@modules/users/infra/typeorm/entities/User';
 
 class FakeUsersRepository implements IUsersRepository {
   private users: User[] = [];
@@ -15,6 +19,18 @@ class FakeUsersRepository implements IUsersRepository {
     const findUser = this.users.find(user => user.email == email);
 
     return findUser;
+  }
+
+  public async findAllProviders({
+    except_user_id,
+  }: IFindAllProvidersDTO): Promise<User[]> {
+    let { users } = this;
+
+    if (except_user_id) {
+      users = this.users.filter(user => user.id !== except_user_id);
+    }
+
+    return users;
   }
 
   public async create(userData: ICreateUserDTO): Promise<User> {

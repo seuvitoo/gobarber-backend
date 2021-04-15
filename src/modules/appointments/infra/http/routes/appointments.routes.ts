01a870a5ea9f87053496147935a7ccd7e8dd26a1
Victor dos Santos <vy.santos@live.com>
2021-04-15 20:25:38 -0300

    [FEATURE] add agenda do prestador

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -3,16 +3,14 @@ import { Router } from 'express';
 import ensureLogin from '@modules/users/infra/http/middlewares/ensureLogin';
 
 import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureLogin);
 appointmentsRouter.post('/', appointmentsController.create);
-
-appointmentsRouter.get('/', async (request, response) => {
-  const appointments = await appointmentsRepository.find();
-  return response.json(appointments);
-});
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;

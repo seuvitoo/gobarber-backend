62df164f7b75cd0d78a82c477be107fcad18f016
Victor Santos <vy.santos@live.com>
2021-07-07 19:49:19 -0300

    [feature] configurando bucket aws

--- src/shared/container/providers/StoreProvider/index.ts
+++ src/shared/container/providers/StoreProvider/index.ts
@@ -1,13 +1,17 @@
 import { container } from 'tsyringe';
 
+import uploadConfig from '@config/upload';
+
 import DiskStorageProvider from './implementations/DiskStoreProvider';
+import S3StorageProvider from './implementations/S3StorageProvider';
 import IStorageProvider from './models/IStoreProvider';
 
 const providers = {
   disk: DiskStorageProvider,
+  s3: S3StorageProvider,
 };
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
-  providers.disk,
+  providers[uploadConfig.driver],
 );

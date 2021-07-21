c88c92d0fa074476fc8d33da9671809c874922d4
Victor Santos <vy.santos@live.com>
2021-07-21 19:41:28 -0300

    [FEATURE] adicionando limite de requisicoes

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -8,6 +8,8 @@ import 'express-async-errors';
 
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
+import rateLimiter from './middleware/rateLimiter';
+
 import routes from './routes';
 
 import '@shared/infra/typeorm';
@@ -15,6 +17,7 @@ import '@shared/container';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
